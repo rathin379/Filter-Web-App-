0ac2bc499cc6b36be04753b655e06f584ee6021b
rathin379 <77762779+rathin379@users.noreply.github.com>
2021-09-19 14:12:20 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -5,12 +5,30 @@ function preload(){
 function setup(){
     canvas = createCanvas(500, 500);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(500, 500);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
 }
 
 function draw(){
-
+    image(video, 0, 0, 500, 500)
 }
 
 function take_snapshot(){
     save('myFilterImage.png');
+}
+
+function modelLoaded(){
+    console.log("Posenet is Initialized")
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("moustache x = " + results[0].pose.moustache.x);
+        console.log("moustache y = " + results[0].pose.moustache.y);
+    }
 }
